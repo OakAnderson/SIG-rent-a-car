e1a50293e0682d83cdaf0ec4642508fc92bd06fc
Anderson Carvalho <andersonfelipe01@live.com>
2019-08-26 16:22:20 -0300

    Script para compilar o programa

---
+++ cria_executavel.py
@@ -0,0 +1,12 @@
+from os import system as sys
+
+sys("""
+    gcc -c cliente.c
+    gcc -c menu.c
+    gcc -c mylib.c
+    gcc -c validacoes.c
+    gcc -c veiculo.c
+    gcc -o exe main.c cliente.o menu.o mylib.o validacoes.o veiculo.o
+    rm *.o
+    
+""")
--- main.c
+++ main.c
@@ -13,25 +13,16 @@
     #include "veiculo.h"
 #endif
 
-/* Comando para compilação:
-gcc -c cliente.c
-gcc -c menu.c
-gcc -c mylib.c
-gcc -c validacoes.c
-gcc -c veiculo.c
-gcc -o exe main.c cliente.o menu.o mylib.o validacoes.o veiculo.o
-
-*/
-
 
 int main ( void ){
     char* a;
 
+    system("clear");
     printf("\n_ _ _ _ _ LOGIN _ _ _ _ _\n\n");
 
     a = entr_str("Usuário: ");
     a = entr_str("Senha: ");
-    
+
     free(a);
 
     inicia_programa();
