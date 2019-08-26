41a8f50e964f38e83989d4a597930ad457995b78
Anderson Carvalho <andersonfelipe01@live.com>
2019-08-26 11:12:44 -0300

    Script para compilaçao no cabeçalho

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
-#include "mylib.h"
-#include "validacoes.h"
+#include "menu.h"
+
+/* Comando para compilação:
+gcc -c cliente.c
+gcc -c menu.c
+gcc -c mylib.c
+gcc -c validacoes.c
+gcc -c veiculo.c
+gcc -o exe main.c cliente.o menu.o mylib.o validacoes.o veiculo.o
+
+*/
 
 
 int main ( void ){
